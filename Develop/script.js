66836ec535ed749c8d02c9c5ace45db74ac21a37
Elham Hamzapour <ehamzapour@gmail.com>
2022-09-01 15:21:47 -0500

    added save button, loop, now working on if, else statements for the time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
 //Today's date
 var today = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(today);
+
+$(document).ready(function(){
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time,text);
+    })
+
+    function timeTracker() {
+        var currentTime = moment().hour();
+        
+    }
+})
